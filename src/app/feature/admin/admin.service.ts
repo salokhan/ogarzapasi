0cd6080202a866d51074de7588765afd10244eec
Solmon <im.solomon@outlook.com>
2020-10-20 20:43:43 +0500

    invalid file removed

--- src/app/feature/admin/admin.service.ts
+++ src/app/feature/admin/admin.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
-import { stat } from 'fs';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({
